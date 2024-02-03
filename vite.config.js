ec5555f418f8bffbf32ae190b164cfd71f28ccce
Kevin Molid <kevinmolid@gmail.com>
2024-02-03 23:13:52 +0100

    Trying tto fix vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
-import {defineConfig} from 'vite'
-
+import { defineConfig } from 'vite';
+import ViteHtmlPlugin from 'vite-plugin-html';
 
 export default defineConfig({
-	plugins: []	
-})
+  plugins: [
+    ViteHtmlPlugin({
+      inject: {
+        injectData: {
+          pages: ['index', 'project-1', 'project-2', 'project-3'],
+        },
+      },
+    }),
+  ],
+  build: {
+    outDir: 'dist',
+  },
+});
